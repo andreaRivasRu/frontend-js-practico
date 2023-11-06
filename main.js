42bc5d95d92acae42456c94211613b79515a621c
Andrea Rivas <andrerivas25@gmail.com>
2023-11-06 17:01:47 -0300

    Mostrar detalles de producto de manera dinamica

--- main.js
+++ main.js
@@ -55,10 +55,34 @@ function toggleAsideShoppingCart() {
     asideShoppingCart.classList.toggle('inactive')
 }
 
-function openProductDetailAside() {
+function displayInfoInProductDetail(event) {
+    const productCard = event.target.closest('.product-card');
+    if (!productCard) return; // Si no se hace clic en un producto, no hagas nada
+
+    const selectedProduct = productList.find((product) => product.name === productCard.querySelector('div p:nth-child(2)').textContent);
+
+    const productDetailImg = productDetailContainer.querySelector('img[id="product_detail_img"]');
+    productDetailImg.setAttribute('src', selectedProduct.image);
+    productDetailImg.setAttribute('alt', selectedProduct.name);
+
+    const productDetailPrice = productDetailContainer.querySelector('p[id="product_detail_price"]');
+    productDetailPrice.innerText = '$' + selectedProduct.price;
+
+    const productDetailName = productDetailContainer.querySelector('p[id="product_detail_name"]');
+    productDetailName.innerText = selectedProduct.name;
+
+    productDetailContainer.classList.remove('inactive');
+}
+
+function openProductDetailAside(event) {
+    
+    displayInfoInProductDetail(event);
+
+    desktopMenu.classList.add('inactive');
     asideShoppingCart.classList.add('inactive');
 
     productDetailContainer.classList.remove('inactive');
+
 }
 
 function closeProductDetailAside() {
@@ -70,12 +94,12 @@ const productList = [];
 productList.push({
     name: 'Racing bicycle',
     price: 170,
-    image: 'https://images.unsplash.com/photo-1583227122027-d2d360c66d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80'
+    image: 'https://images.unsplash.com/photo-1576435728678-68d0fbf94e91?auto=format&fit=crop&q=80&w=1948&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 })
 productList.push({
     name: 'Mountain Bike',
     price: 200,
-    image: 'https://images.unsplash.com/photo-1583227122027-d2d360c66d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80'
+    image: 'https://images.unsplash.com/photo-1618762044398-ec1e7e048bbd?auto=format&fit=crop&q=80&w=1827&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 })
 productList.push({
     name: 'Bicycle touring',
@@ -83,19 +107,19 @@ productList.push({
     image: 'https://images.unsplash.com/photo-1583227122027-d2d360c66d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80'
 })
 productList.push({
-    name: 'Bicycle basket',
+    name: 'Basket',
     price: 50,
-    image: 'https://images.unsplash.com/photo-1583227122027-d2d360c66d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80'
+    image: 'https://images.unsplash.com/photo-1541659298873-20f29dc26c0c?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 })
 productList.push({
     name: 'Helmet',
     price: 60,
-    image: 'https://images.unsplash.com/photo-1583227122027-d2d360c66d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80'
+    image: 'https://images.unsplash.com/photo-1601971360277-7b4c8aa60894?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 })
 productList.push({
     name: 'Lights',
     price: 30,
-    image: 'https://images.unsplash.com/photo-1583227122027-d2d360c66d3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80'
+    image: 'https://images.unsplash.com/photo-1570106196361-2daac379ec3f?auto=format&fit=crop&q=80&w=1851&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 })
 
 function renderProducts(arr) {
